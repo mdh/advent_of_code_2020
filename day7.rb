1da46c140ec74c2f4077b14c5ce4596abcdd8233
Marek de Heus <marek@deheus.net>
2020-12-07 14:45:48 +0100

    Added solution for day 7

--- day7.rb
+++ day7.rb
@@ -13,5 +13,22 @@ while true do
   break if (new_bags - bags).size == 0
   bags |= new_bags
 end
-puts bags.size - 1
+
+bags.size - 1
+
 # part 2
+@rules = {}
+input.lines.each do |line|
+  k1, k2, _ = line.split(' ', 3)
+  key = k1 + k2
+  values = line.split(/bag[s]{0,1}/).map {|bag| bag.split[-3,3]}.compact.map { |number, *bag| [number, bag.join] }
+  @rules[key] = values
+end
+
+def number_of_bags(bag)
+  @rules[bag].map { |number, bag_name|
+    bag_name == 'noother' ? 0 : number.to_i + (number.to_i * (number_of_bags(bag_name) ))
+  }.reduce(:+)
+end
+
+number_of_bags('shinygold')
