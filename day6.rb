f5730512ba8a4295ee37ba263aca57fe1dc5f37d
Marek de Heus <marek@deheus.net>
2020-12-07 11:34:19 +0100

    Added solution for day 6, pt 2

--- day6.rb
+++ day6.rb
@@ -2,3 +2,6 @@ input = File.read('day6_input.txt')
 
 # part 1
 input.split(/\n\n/).map { |txt| txt.chars.grep(/\w/).uniq.size }.reduce :+
+
+# part 2
+input.split(/\n\n/).map { |txt| txt.strip.lines.map {|line| line.chars.grep(/\w/) }.reduce(:&).size }.reduce(:+)
