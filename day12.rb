33c3d4ad08313bacb660b43cab9d8427ba59a23b
Marek de Heus <marek@deheus.net>
2020-12-12 21:54:47 +0100

    Add day 12 solution, part 2

--- day12.rb
+++ day12.rb
@@ -8,6 +8,14 @@ R90
 F11
 EOT
 
+commands =
+  input.strip.lines.map do |line|
+    line    = line.chars
+    command = line.shift
+    value   = line.join.to_i
+    [command, value]
+  end
+
 # part 1
 Ship = Struct.new(:direction, :north, :west) do
   def f(n)
@@ -53,14 +61,7 @@ Ship = Struct.new(:direction, :north, :west) do
   end
 
   def l(degrees)
-    (degrees / 90).times do
-      self.direction = {
-        north: :west,
-        west:  :south,
-        south: :east,
-        east:  :north
-      }[self.direction]
-    end
+    r(360 - degrees)
   end
 
   def manhattan
@@ -68,16 +69,52 @@ Ship = Struct.new(:direction, :north, :west) do
   end
 end
 
-commands =
-  input.lines.map do |line|
-    line    = line.chars
-    command = line.shift
-    value   = line.join.to_i
-    [command, value]
-  end
-
 ship = Ship.new :east, 0, 0
 commands.each do |command, value|
   ship.send command.downcase, value
 end
 puts ship.manhattan
+
+# part 2
+Ship2 = Struct.new(:waypoint, :north, :west) do
+  def f(n)
+    self.north += (n * waypoint.north)
+    self.west  += (n * waypoint.west)
+  end
+
+  def n(value)
+    waypoint.north += value
+  end
+
+  def s(value)
+    waypoint.north -= value
+  end
+
+  def w(value)
+    waypoint.west += value
+  end
+
+  def e(value)
+    waypoint.west -= value
+  end
+
+  def r(degrees)
+    (degrees / 90).times do
+      self.waypoint = Waypoint.new(waypoint.west, -waypoint.north)
+    end
+  end
+
+  def l(degrees)
+    r(360 - degrees)
+  end
+
+  def manhattan
+    north.abs + west.abs
+  end
+end
+
+Waypoint = Struct.new(:north, :west)
+
+ship = Ship2.new Waypoint.new(1, -10), 0, 0
+commands.each { |command, value| ship.send command.downcase, value }
+pp ship.manhattan
