02acb9e63c32469980967350990da9f6a14221cf
Marek de Heus <marek@deheus.net>
2020-12-15 08:55:15 +0100

    Add day 15 part 2

--- day15.rb
+++ day15.rb
@@ -25,3 +25,22 @@ while numbers.size < 2020 do
   occurrences[new_number] += 1
 end
 pp numbers.last
+
+# part 2
+numbers = input.split(',').map(&:to_i)
+occurrences = Hash.new { [] }
+numbers.each.with_index { |number, index| occurrences[number] = occurrences[number] << index }
+while numbers.size < 30_000_000 do
+  last_number = numbers.last
+  new_number =
+    if occurrences[last_number].size == 1
+      0
+    else
+      prev, last = occurrences[last_number][-2,2]
+      last - prev
+    end
+  occurrences[new_number] = occurrences[new_number] << numbers.size
+  numbers << new_number
+end
+pp numbers.last
+
