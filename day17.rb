2e33fee0e49a4ac9eb43c54cfcde95213d50d423
Marek de Heus <marek@deheus.net>
2020-12-17 19:48:43 +0100

    Add day 17 pt2 bit faster version

--- day17.rb
+++ day17.rb
@@ -7,23 +7,10 @@ test_input = <<EOT
 ###
 EOT
 
-# part 1
-Cube = Struct.new(:x, :y, :z, :w, :active) do
-  include Comparable
-
-  def <=>(other)
-    [x, y, z, w] <=> [other.x, other.y, other.z, other.w]
-  end
-
-  def hash
-    x + y + z + w
-  end
-end
-
-grid = Set.new
-input.lines.map.with_index do |line, x|
-  line.strip.chars.map.with_index do |c, y|
-    grid << Cube.new(x, y, 0, 0, (c == '#'))
+grid = {}
+input.lines.each.with_index do |line, x|
+  line.strip.chars.each.with_index do |c, y|
+    grid[[x, y, 0, 0]] = (c == '#')
   end
 end
 
@@ -31,34 +18,33 @@ def relative_neighbour_coords
   @relative_coords ||= [-1,-1,-1,-1,0,0,0,0,1,1,1,1].permutation(4).uniq - [[0,0,0,0]]
 end
 
-def neighbours(grid, cube)
+def neighbours(grid, coords)
+  results = {}
   relative_neighbour_coords.map do |dx,dy,dz,dw|
-    grid.find do |other|
-      (cube.x + dx == other.x) && (cube.y + dy == other.y) && (cube.z + dz == other.z) && (cube.w + dw == other.w)
-    end || Cube.new(cube.x + dx, cube.y + dy, cube.z + dz, cube.w + dw, false)
+    x,y,z,w = [coords[0] + dx, coords[1] + dy, coords[2] + dz, coords[3] + dw]
+    results[[x,y,z,w]] = grid[[x,y,z,w]] || false
   end
+  results
 end
 
 6.times do |cycle|
-  new_grid = Set.new
-  grid.each do |cube|
+  new_grid = {}
+  grid.each do |coords, value|
     # determine state for all neighbours
-    neighbours(grid, cube).each do |neighbour|
+    neighbours(grid, coords).each do |neighbour_coords, value2|
       active_count = 0
-      neighbours(grid, neighbour).each do |c|
+      neighbours(grid, neighbour_coords).each do |_, value3|
         break if active_count > 3
-        active_count += 1 if c.active
+        active_count += 1 if value3
       end
       # add neighbours to new_grid
-      if (neighbour.active && [2,3].include?(active_count)) ||
-      (!neighbour.active && active_count == 3)
-        new_cube = Cube.new *neighbour.to_h.values
-        new_cube.active = true
-        new_grid << new_cube
+      if (value2 && [2,3].include?(active_count)) ||
+      (!value2 && active_count == 3)
+        new_grid[neighbour_coords] = true
       else # skip
       end
     end
   end
-  puts "After cycle #{cycle + 1}: #{ new_grid.count }"
+  puts "After cycle #{cycle + 1}: #{ new_grid.size }"
   grid = new_grid
 end
